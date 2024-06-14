6fbf040f9dab2dcf25fa7946d0f1cca2957f59ac
sathihskuamr M <msksathishzz@gmail.com>
2024-06-14 23:43:06 +0530

    babel setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components/*',
+          '@images': './src/images/*',
+          '@navigation/*': './src/navigation/*',
+          // '@hooks': './src/hooks/',
+        },
+      },
+    ],
+  ],
 };
