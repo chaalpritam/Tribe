6d199a38dba23bd1cd1d1b74df375137f737de51
Chaal Pritam <chaalpritam@gmail.com>
2026-05-17 15:46:31 +0530

    Initial Commit

---
+++ Tribe/TribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TribeApp.swift
+//  Tribe
+//
+//  Created by chaalpritam on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
